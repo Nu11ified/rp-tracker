db0c96dc18ca27724dbed17387fd7e91aad5f961
Nu11ified <86795298+Nu11ified@users.noreply.github.com>
2024-12-15 16:37:37 -0600

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Null@JRP'
+description 'Enhanced RP Queue System with NUI'
+version '1.1.0'
+
+server_script 'config.lua'
+server_script 'server.lua'
+
+client_script 'client.lua'
+
+ui_page 'ui/index.html'
+
+files {
+  'ui/index.html',
+  'ui/style.css',
+  'ui/script.js'
+}
